508a80303f53592c1e6e0359ea270d2143ba41d6
student <student@alextest-99.uncanet.unca.edu>
2017-06-12 09:03:38 -0400

    Actually added files, that might help

---
+++ differentKindsOfDataEvaluation.rkt
@@ -0,0 +1,5 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname differentKindsOfDataEvaluation) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(and (or (= (string-length "hello world") (string->number "11"))
+(string=? "hello world" "good morning")) (>= (+ (string-length "hello world") 60) 80))
